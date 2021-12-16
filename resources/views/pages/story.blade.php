8a3ea4bb9af7d1d3725566b5eaced6f84640a8df
Andreas Licht <alicht83@gmail.com>
2021-12-16 16:50:29 +0100

    work on layout and content

--- resources/views/pages/story.blade.php
+++ resources/views/pages/story.blade.php
@@ -6,7 +6,7 @@
     <main>
         <!-- Intro/ Hero -->
         <section class="container min-h-screen px-4 py-8 mx-auto space-y-8 bg-gray-200">
-            <div class="aspect-w-1 aspect-h-1">
+            <div class="overflow-hidden rounded-md aspect-w-1 aspect-h-1">
                 <img
                     class="object-cover grayscale"
                     src="/img/familie-mit-baby.jpg"
@@ -24,14 +24,14 @@
             </div>
 
             <div class="flex justify-center gap-4 py-8">
-                <a href="#" class="w-full px-4 py-2 text-center text-white bg-gray-500 border-2 border-gray-500 rounded-md">Kurs buchen</a>
-                <a href="#" class="w-full px-4 py-2 text-center text-gray-700 bg-white border-2 border-gray-500 rounded-md">Mehr erfahren</a>
+                <a href="#" class="w-full px-4 py-2 font-semibold text-center text-white bg-gray-500 border-2 border-gray-500 rounded-md">Kurs buchen</a>
+                <a href="#" class="w-full px-4 py-2 font-semibold text-center text-gray-700 border-2 border-gray-500 rounded-md">Mehr erfahren</a>
             </div>
         </section>
 
         <!-- Vorstellung Katja -->
         <section class="container min-h-screen px-4 py-8 mx-auto space-y-8">
-            <div class="aspect-w-1 aspect-h-1">
+            <div class="overflow-hidden rounded-md aspect-w-1 aspect-h-1">
                 <img 
                     class="object-cover grayscale"
                     src="/img/katja.jpg" 
@@ -47,13 +47,13 @@
             </div>
 
             <div class="flex justify-center gap-4 py-8">
-                <a href="#" class="w-full px-4 py-2 text-center text-gray-700 bg-white border-2 border-gray-500 rounded-md">Mehr über mich</a>
+                <a href="#" class="w-full px-4 py-2 font-semibold text-center text-gray-700 border-2 border-gray-500 rounded-md">Mehr über mich</a>
             </div>
         </section>
 
         <!-- Yoga Prenatal -->
         <section class="container min-h-screen px-4 py-8 mx-auto space-y-8 bg-gray-200">
-            <div class="aspect-w-1 aspect-h-1">
+            <div class="overflow-hidden rounded-md aspect-w-1 aspect-h-1">
                 <img 
                     class="object-cover grayscale"
                     src="/img/katja-schwanger.jpg" 
@@ -65,8 +65,8 @@
                 <h2 class="max-w-3xl text-2xl text-gray-700 uppercase xl:text-6xl font-accent">
                     Yoga für Schwangere
                 </h2>
-                <p class="block text-xl text-gray-500 uppercase">Eine Schwangerschaft ist der Beginn einer wundervollen Reise. Eine Reise voller Veränderungen.</p>
-                <p>Yoga bereitet dich liebevoll auf diese Veränderungen vor.</p>
+                <p class="block text-xl text-gray-500 uppercase">Eine Schwangerschaft ist der Beginn einer wundervollen Reise.</p>
+                <p>Eine Reise voller Veränderungen. Yoga bereitet dich liebevoll auf diese Veränderungen vor.</p>
                 <p>Mit geburtsvorbereitenden Informationen, Atem- und Entspannungstechniken begleite ich Dich auf dieser Reise.</p>
                 <h3>Kursmerkmale</h3>
                 <ul>
@@ -78,18 +78,18 @@
             </div>
 
             <div class="flex justify-center gap-4 py-8">
-                <a href="#" class="w-full px-4 py-2 text-center text-white bg-gray-500 border-2 border-gray-500 rounded-md">Kurs buchen</a>
-                <a href="#" class="w-full px-4 py-2 text-center text-gray-700 bg-white border-2 border-gray-500 rounded-md">Mehr erfahren</a>
+                <a href="#" class="w-full px-4 py-2 font-semibold text-center text-white bg-gray-500 border-2 border-gray-500 rounded-md">Kurs buchen</a>
+                <a href="#" class="w-full px-4 py-2 font-semibold text-center text-gray-700 border-2 border-gray-500 rounded-md">Mehr erfahren</a>
             </div>
         </section>
 
         <!-- Yoga Postnatal -->
         <section class="container min-h-screen px-4 py-8 mx-auto space-y-8">
-            <div class="aspect-w-1 aspect-h-1">
+            <div class="overflow-hidden rounded-md aspect-w-1 aspect-h-1">
                 <img 
                     class="object-cover grayscale"
-                    src="/img/katja-schwanger.jpg" 
-                    alt="Junge Familie mit Baby"
+                    src="/img/rueckbildung.jpg" 
+                    alt="Junge Mutter mit Baby auf dem Arm"
                 >
             </div>
 
@@ -97,8 +97,9 @@
                 <h2 class="max-w-3xl text-2xl text-gray-700 uppercase xl:text-6xl font-accent">
                     Yoga zur Rückbildung
                 </h2>
-                <p class="block text-xl text-gray-500 uppercase">Wenn du dein Baby endlich im Arm halten darfst, unterstützt Yoga dich wundervoll in der Rückbildung und dem Hineinwachsen in die Mama-Rolle.</p>
-                <p>Mit Impulse aus den Bereichen bedürfnisorientierte Erziehung und Achtsamkeit, die dich in der Beziehung zu dir selbst, deiner neuen Rolle als Mama und zu deinem Baby bestärken und mit jeder Menge kräftigenden Übungen darfst du körperlich und mental zu deiner starken Mitte finden.</p>
+                <p class="block text-xl text-gray-500 uppercase">Wenn du dein Baby endlich im Arm halten darfst.</p>
+                <p>Yoga kann Dich in diesem Moment wundervoll unterstützen. Sei es in der Rückbildung oder beim Hineinwachsen in die Mama-Rolle.</p>
+                <p>Mit Impulsen aus den Bereichen bedürfnisorientierte Erziehung und Achtsamkeit, die Dich in der Beziehung zu dir selbst, Deiner neuen Rolle als Mama und zu deinem Baby bestärken und mit jeder Menge kräftigenden Übungen darfst du körperlich und mental zu deiner starken Mitte finden.</p>
                 <h3>Kursmerkmale</h3>
                 <ul>
                     <li>Ausschließlich Präsenzveranstaltungen</li>
@@ -109,8 +110,70 @@
             </div>
 
             <div class="flex justify-center gap-4 py-8">
-                <a href="#" class="w-full px-4 py-2 text-center text-white bg-gray-500 border-2 border-gray-500 rounded-md">Kurs buchen</a>
-                <a href="#" class="w-full px-4 py-2 text-center text-gray-700 bg-white border-2 border-gray-500 rounded-md">Mehr erfahren</a>
+                <a href="#" class="w-full px-4 py-2 font-semibold text-center text-white bg-gray-500 border-2 border-gray-500 rounded-md">Kurs buchen</a>
+                <a href="#" class="w-full px-4 py-2 font-semibold text-center text-gray-700 border-2 border-gray-500 rounded-md">Mehr erfahren</a>
+            </div>
+        </section>
+
+        <!-- Yoga für Kinder -->
+        <section class="container min-h-screen px-4 py-8 mx-auto space-y-8 bg-gray-200">
+            <div class="overflow-hidden rounded-md aspect-w-1 aspect-h-1">
+                <img 
+                    class="object-cover grayscale"
+                    src="/img/kinder-yoga.jpg" 
+                    alt="Katja mit Kindern beim Yoga"
+                >
+            </div>
+
+            <div class="prose">
+                <h2 class="max-w-3xl text-2xl text-gray-700 uppercase xl:text-6xl font-accent">
+                    Yoga für Kinder
+                </h2>
+                <p class="block text-xl text-gray-500 uppercase">Lass dich nicht unterkriegen, sei frech und wild und wunderbar.</p>
+                <p>Kinderyoga macht in erster Linie ganz viel Spaß und Freude. Neben Körperhaltungen werden Entspannungsübungen ebenso wie Massagen und Phantasiereisen liebevoll durchgeführt. Vor allem aber auch Achtsamkeit und Körperwahrnehmung können Kinder mit Yoga erlernen. Jede Stunde ist eine Abenteuerreise, in der sich die Kinder verwandeln und einfühlen dürfen.</p>
+    
+                <h3>Galerie</h3>            
+            </div>
+
+            <div class="grid grid-cols-3 gap-2">
+                <div class="overflow-hidden rounded-md aspect-w-4 aspect-h-3">
+                    <img
+                        class="object-cover grayscale"
+                        src="/img/2.jpg"
+                        alt=""
+                    >
+                </div>
+
+                <div class="overflow-hidden rounded-md aspect-w-4 aspect-h-3">
+                    <img
+                        class="object-cover grayscale"
+                        src="/img/4.jpg"
+                        alt=""
+                    >
+                </div>
+
+                <div class="overflow-hidden rounded-md aspect-w-4 aspect-h-3">
+                    <img
+                        class="object-cover grayscale"
+                        src="/img/5.jpg"
+                        alt=""
+                    >
+                </div>
+            </div>
+
+            <div class="prose">
+                <h3>Kursmerkmale</h3>
+                <ul>
+                    <li>Ausschließlich Präsenzveranstaltungen</li>
+                    <li>Kleingruppen bis 8 Personen</li>
+                    <li>Findet in 8er Blöcken statt</li>
+                    <li>Persönliche und vertraute Atmosphäre</li>
+                </ul>
+            </div>
+
+            <div class="flex justify-center gap-4 py-8">
+                <a href="#" class="w-full px-4 py-2 font-semibold text-center text-white bg-gray-500 border-2 border-gray-500 rounded-md">Kurs buchen</a>
+                <a href="#" class="w-full px-4 py-2 font-semibold text-center text-gray-700 border-2 border-gray-500 rounded-md">Mehr erfahren</a>
             </div>
         </section>
 
@@ -119,7 +182,27 @@
         </section>
     </main>
 
+    <aside class="container px-4 py-8 mx-auto bg-gray-300">
+        <h2 class="text-lg">
+            Bleibe auf dem Laufenden und erhalte 10% Rabatt auf Deinen nächsten Kurs!
+        </h2>
+
+        <form 
+            class="mt-4"
+            action="#"
+        >
+            <input 
+                class="w-full overflow-hidden bg-gray-100 rounded-md"
+                type="text"
+                name=""
+                id=""
+                placeholder="Deine Email Adresse"
+            >
+            
+            <button class="w-full px-4 py-2 mt-2 font-semibold text-center text-white bg-gray-500 border-2 border-gray-500 rounded-md">Newsletter abonnieren</button>
+        </form>
+    </aside>
+
     <footer class="bg-gray-100">
-        <h2>Footer</h2>
     </footer>
 </x-guest-layout>
